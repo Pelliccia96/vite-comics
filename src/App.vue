4cac5691ebc83dcc85a5d0e85627cc5c6278edfa
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-12-02 16:23:44 +0100

    Adds TheMain.vue (main section)

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <TheHeader></TheHeader>
+  <TheMain></TheMain>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import TheMain from "./components/TheMain.vue";
 
 export default {
-  components: { TheHeader }
+  components: { TheHeader, TheMain }
 }
 </script>
 
---
+++ src/components/TheMain.vue
@@ -0,0 +1,17 @@
+<template>
+    <main class="bg-dark">
+        <div class="container">
+            <div class="text-white py-5">
+                <h2>--&#62; Content goes here &#60;--</h2>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default{
+}
+</script>
+
+<style scoper>
+</style>

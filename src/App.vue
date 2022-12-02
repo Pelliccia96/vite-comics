3711863ee09446942c3d8614e8449d7d84f1c0df
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-12-02 18:10:04 +0100

    Adds TheFooter.vue (footer list)

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,17 @@
   <TheHeader></TheHeader>
   <TheMain></TheMain>
   <TheMainBanner></TheMainBanner>
+  <TheFooter></TheFooter>
 </template>
 
 <script>
 import TheHeader from "./components/TheHeader.vue";
 import TheMain from "./components/TheMain.vue";
 import TheMainBanner from "./components/TheMainBanner.vue";
+import TheFooter from "./components/TheFooter.vue";
 
 export default {
-  components: { TheHeader, TheMain, TheMainBanner, }
+  components: { TheHeader, TheMain, TheMainBanner, TheFooter }
 }
 </script>
 
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,36 @@
+<template>
+    <footer class="bg-footer">
+        <div class="container">
+            <div class="text-white">
+                <div>
+                    <ul>
+                        <li>
+                            <h4>DC Comics</h4>
+                            <div v-for="itemList in footerList">
+                                <a href="#">{{itemList}}</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            footerList: ["Characters", "Comics", "Movies", "TV", "Games", "Videos", "News"],
+        }
+    },
+}
+</script>
+
+<style scoped>
+.bg-footer {
+    background-image: url("../../public/img/footer-bg.jpg");
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>

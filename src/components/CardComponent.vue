f83c89b8f9a2f05a2421a71a0460d04450b94764
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-12-05 16:12:19 +0100

    Completed TheMain.vue Coding

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="card border-0">
+        <img :src="card.thumb" class="card-img-top">
+        <div class="card-body bg-dark">
+            <h5 class="card-title text-white">{{card.series}}</h5>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        card: {
+            type: Object,
+            required: true,
+        }
+    },
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,16 +1,108 @@
 <template>
     <main class="bg-dark">
-        <div class="main-bg"></div>
+        <!-- Jumbotron -->
+        <div class="main-bg"/>
         <div class="container">
             <div class="text-white d-flex margin-">
                 <h3 class="border border-primary bg-primary rounded fw-bold p-3">CURRENT SERIES</h3>
+            </div>
+            <!-- Cards -->
+            <div class="row row-cols-1 row-cols-md-3 row-cols-lg-6 g-4 my-3">
+                <div class="col" v-for="(singleCard,i) in comicsList" :key="(singleCard.series + i)">
+                    <CardComponent :card="singleCard"></CardComponent>
+                </div>
+            </div>
+            <div class="text-center">
+                <button class="btn btn-primary fw-bold p-2 px-5 my-4">LOAD MORE</button>
             </div>
         </div>
     </main>
 </template>
 
 <script>
+import CardComponent from './CardComponent.vue';
+
 export default{
+    components: { CardComponent },
+    data() {
+        return {
+            comicsList:
+            [
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ]
+        }
+    },
 }
 </script>
 
@@ -24,5 +116,10 @@ export default{
 
 .margin- {
     margin-top: -30px;
+}
+
+img {
+    aspect-ratio: 1 / 1;
+    object-fit: cover;
 }
 </style>
